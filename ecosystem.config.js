f6c6ce5edc4c0b2912742d403196b7ecfebddaa3
ymfl@1917199677250378 <395041293@qq.com>
2019-05-30 16:48:24 +0800

    --

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,10 @@ module.exports = {
   apps : [{
     name: 'nestPro',
     script: 'npm run prestart:prod',
-
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '500M',
-    port: 3001,
     env: {
       NODE_ENV: 'development',
     },
