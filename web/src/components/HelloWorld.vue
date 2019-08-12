d6744b0e0e5756825601ebbf39d5faeb9c7ef739
ymfl@1917199677250378 <395041293@qq.com>
2019-08-12 15:11:30 +0800

    --

--- web/src/components/HelloWorld.vue
+++ web/src/components/HelloWorld.vue
@@ -1,48 +1,45 @@
 <template>
-  <div class="hello">
-    <!--<img :src="captcha" alt="" @click="captchaClickHandler">-->
-    <embed :src="base64"  class="haha" />
-  </div>
+    <div class="hello">
+        <van-button type="default" size="small">默认按钮</van-button>
+        <img :src="captcha" alt="" @click="captchaClickHandler">
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data(){
-    return {
-      captcha:'http://localhost:3061/public/captcha',
-      base64:''
-    }
-  },
-  props: {
-    msg: String
-  },
-  methods:{
-    captchaClickHandler(){
-      this.captcha=this.captcha.split('?')[0]+'?'+Date.parse(new Date())
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        captcha: 'http://localhost:3061/public/captcha',
+        base64: ''
+      }
+    },
+    props: {},
+    methods: {
+      captchaClickHandler() {
+        this.captcha = this.captcha.split('?')[0] + '?' + Date.parse(new Date())
+      }
+
     }
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .haha{
-    width: 100%;
-    height: 400px;
-  }
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
